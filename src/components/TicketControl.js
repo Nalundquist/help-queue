e1329093e9246361d012e20879e5d20dc2b0f42a
Noah Lundquist <nalundquist@gmail.com>
2022-11-15 10:46:42 -0800

    add liminal confirm pages before form
    Co-authored-by: Ted Gold <ted.jonathan.gold@gmail.com>

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -7,23 +7,36 @@ class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      formVisibleOnPage: 0
     };
   }
 
   handleClick = () => {
-    this.setState(prevState => ({
-      formVisibleOnPage: !prevState.formVisibleOnPage
-    }));
+		if (this.state.formVisibleOnPage === 4){
+			this.setState({formVisibleOnPage: 0});
+		} else {
+			this.setState(prevState => ({
+				formVisibleOnPage: prevState.formVisibleOnPage + 1
+			}));
+		}
   }
 
   render(){
     let currentlyVisibleState = null;
     let buttonText = null;
-    if (this.state.formVisibleOnPage) {
+    if (this.state.formVisibleOnPage === 1) {
+			currentlyVisibleState = <p>Have you gone through all the steps on the Learn How to Program debugging lesson?</p>
+			buttonText = "Yeth";
+		}	else if (this.state.formVisibleOnPage === 2){
+			currentlyVisibleState = <p>Are you a little tiny baby?</p>
+			buttonText = "Goo goo ga ga";
+		} else if (this.state.formVisibleOnPage === 3){
+			currentlyVisibleState = <p>What's your favorite food?</p>
+			buttonText = "Spaghetti";
+		} else if (this.state.formVisibleOnPage === 4){
       currentlyVisibleState = <NewTicketForm />
       buttonText = "Return to Ticket List";
-    } else {
+		} else {
       currentlyVisibleState = <TicketList />
       buttonText = "Add Ticket";
     }
